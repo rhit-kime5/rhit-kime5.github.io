f401d1070efffff621d942795cd33d50b97e7e4a
rhit-kime5 <78507856+rhit-kime5@users.noreply.github.com>
2021-07-11 15:44:39 -0400

    added template

---
+++ prev/scripts/main.js
@@ -0,0 +1,40 @@
+/**
+ * @fileoverview
+ * Provides the JavaScript interactions for all pages.
+ *
+ * @author 
+ * Edward Kim
+ */
+//
+
+var rhit = rhit || {};	//	namespace
+
+rhit.variableName = "";	//	globals
+
+
+//	function syntax example
+rhit.functionName = function () {
+
+};
+
+
+//	class syntax example
+rhit.ClassName = class {
+	constructor() {
+
+	}
+
+	methodName() {
+
+	}
+}
+
+
+//	main function
+rhit.main = function () {
+	console.log("Ready");
+};
+
+
+//	main function cal
+rhit.main();
