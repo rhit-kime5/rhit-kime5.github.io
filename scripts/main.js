c5b7f5e7a2d0defe6e6d22dcb7ac9ab0f999bd60
rhit-kime5 <78507856+rhit-kime5@users.noreply.github.com>
2022-01-06 19:04:36 -0500

    completed darpa and minerl pages

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 var controller = new ScrollMagic.Controller();
 
-var revealElements = document.getElementsByClassName("reveal");
+var revealElements = document.getElementsByClassName("reveal1");
 	for (var i = 0; i < revealElements.length; i++) { 
 		new ScrollMagic.Scene({
 				triggerElement: revealElements[i], 
@@ -10,5 +10,18 @@ var revealElements = document.getElementsByClassName("reveal");
 				// 50, 80%, 0.9
 			}) 
 			.setClassToggle(revealElements[i], "visible") 
+			.addTo(controller);
+	}
+
+var reveal2Elements = document.getElementsByClassName("reveal2");
+	for (var i = 0; i < reveal2Elements.length; i++) { 
+		new ScrollMagic.Scene({
+				triggerElement: reveal2Elements[i], 
+				offset: 50, 
+				duration: "0",
+				triggerHook: 1,
+			}) 
+			.setClassToggle(reveal2Elements[i], "visible") 
+			.reverse(false)
 			.addTo(controller);
 	}
